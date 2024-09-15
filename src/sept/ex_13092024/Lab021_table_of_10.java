5f86c108fe308a4854ff1fa6f71b5aabc952c6fa
PANKAJG-TECH\Pankaj Gokhale <pankaj.gokhale@rebelfoods.com>
2024-09-16 00:19:11 +0530

    New file added

---
+++ src/sept/ex_13092024/Lab021_table_of_10.java
@@ -0,0 +1,31 @@
+package sept.ex_13092024;
+
+public class Lab021_table_of_10 {
+    public static void main(String[] args) {
+        //Single Char
+
+        char c1='A';
+
+        char c2='B';
+
+        char c3='C';
+
+        //Escape Char
+
+        char new_line='\n';
+
+        char tab_line='\t';
+
+        char back_space='\b';
+
+        char car_r='\r';
+
+        System.out.println("Pramod"+ new_line+"Dutta");
+        System.out.println("Pramod"+ tab_line+"Dutta");
+        System.out.println("Pramod"+ back_space+"Dutta");
+        System.out.println("Pramod"+ car_r+"Dutta");
+
+
+
+    }
+}
