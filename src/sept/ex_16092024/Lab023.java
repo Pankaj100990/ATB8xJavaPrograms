9dd8ee6431084b23a7dea65e8375988b8a84f491
PANKAJG-TECH\Pankaj Gokhale <pankaj.gokhale@rebelfoods.com>
2024-09-25 22:26:31 +0530

    New file added

---
+++ src/sept/ex_16092024/Lab023.java
@@ -0,0 +1,8 @@
+package sept.ex_16092024;
+
+public class Lab023 {
+    public static void main(String[] args) {
+        int Enum=10;
+        System.out.println(Enum);
+    }
+}
---
+++ src/sept/ex_25092024/Lab25.java
@@ -0,0 +1,15 @@
+package sept.ex_25092024;
+
+public class Lab25 {
+
+    public static void main(String[] args) {
+        System.out.println("Even numbers from 0 to 50:");
+
+        for (int i = 0; i <= 50; i++) {
+            if (i % 2 == 0) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
+
