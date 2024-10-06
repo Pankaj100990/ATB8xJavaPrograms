3ca2ae28e6003107debcb93b470f318029860f2e
PANKAJG-TECH\Pankaj Gokhale <pankaj.gokhale@rebelfoods.com>
2024-10-06 15:56:17 +0530

    New file added

--- src/sept/Tasks/Task27sepDowhile.java
+++ src/sept/Tasks/Task27sepDowhile.java
@@ -13,3 +13,26 @@ public class Task27sepDowhile {
      }while(a<10);
  }
 }
+// Pramod Solution:
+// Print the First 5 Even Numbers
+//        int count = 1;
+//        int number = 2;
+//        do {
+//            System.out.println(number);
+//            number +=2;
+//            count++;
+//        }while (count <= 5);
+//    }
+
+//  Solution 2:
+//int count = 0; // To keep track of how many even numbers we've printed
+//int number = 2; // Start from the first even number
+//
+//        do {
+//                if (number % 2 == 0) { // Check if the number is even
+//                System.out.println(number); // Print the current even number
+//                  count++; // Increment the count of even numbers printed
+//        }
+//           number++; // Move to the next number
+//        } while (count < 5); // Continue until we've printed 5 even numbers
+//        }
